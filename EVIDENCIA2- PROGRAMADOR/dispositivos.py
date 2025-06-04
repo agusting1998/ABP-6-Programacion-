4bb81b8a3bf4e84b3dc7082a6492c901783c0bd0
Matias Aranda <matiasaranda426@gmail.com>
2025-06-04 18:58:25 -0300

    Se eliminó el modulo estado y se lo incorporó como funcion integrada en el módulo Dispositivos

--- EVIDENCIA2- PROGRAMADOR/dispositivos.py
+++ EVIDENCIA2- PROGRAMADOR/dispositivos.py
@@ -32,3 +32,21 @@ def buscar_dispositivo(nombre):
 #Funcion para mostrar los dispositivos almacenados
 def obtener_dispositivos():
     return dispositivos
+
+#Función para cambiar el estado del dispositivo (Encendido/Apagado)
+def cambiar_estado(nombre, nuevo_estado):
+    dispositivo = dispositivos.buscar_dispositivo(nombre)
+    if dispositivo:
+        dispositivo['estado'] = nuevo_estado
+        print(f"Estado del dispositivo '{nombre}' cambiado a '{nuevo_estado}'.")
+    else:
+        print(f"No se encontró el dispositivo '{nombre}'.")
+
+#Función que muestra el estado del dispositivo (Encendido/Apagado)
+def ver_estado(nombre):
+    dispositivo = dispositivos.buscar_dispositivo(nombre)
+
+    if dispositivo:
+        print(f"{nombre}: {dispositivo['estado']}")
+    else:
+        print(f"Dispositivo '{nombre}' no encontrado..")
