6800466b1a10e19d55c960466cb217c07da0a1e8
agusting98 <133926923+agusting98@users.noreply.github.com>
2025-06-06 22:52:17 -0300

    se agrega la funcion de "cambiar estado"

--- EVIDENCIA2- PROGRAMADOR/estados.py
+++ EVIDENCIA2- PROGRAMADOR/estados.py
@@ -7,3 +7,12 @@ def ver_estado_dispositivo(nombre):
             print(f"Estado de {nombre}: {dispositivo['estado']}")
             return
     print(f"No se encontró un dispositivo llamado '{nombre}'.")
+
+def cambiar_estado_dispositivo(nombre, nuevo_estado):
+    dispositivos = obtener_dispositivos()
+    for dispositivo in dispositivos:
+        if dispositivo["nombre"] == nombre:
+            dispositivo["estado"] = nuevo_estado
+            print(f"El estado de {nombre} ha sido cambiado a: {nuevo_estado}")
+            return
+    print(f"No se encontró un dispositivo llamado '{nombre}'.")
