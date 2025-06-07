7be2aa874112dc479f1c002558df530d3c915c27
agusting98 <133926923+agusting98@users.noreply.github.com>
2025-06-06 22:35:00 -0300

    Correccion Inicio de Sesion
    - Importar Main
    - Llamar a la funcion del "Menu Main" si iniciamos sesion.

--- EVIDENCIA2- PROGRAMADOR/inicio_sesion.py
+++ EVIDENCIA2- PROGRAMADOR/inicio_sesion.py
@@ -1,3 +1,6 @@
+import main
+import usuario
+
 usuarios = {
     "usuario1": "contraseña123",
     "usuario2": "abc123",
@@ -6,38 +9,33 @@ usuarios = {
 
 def iniciar_sesion():
     print("\n--- Inicio de Sesión ---")
-    usuario = input("Usuario: ")
+    usuario_nombre = input("Usuario: ")
     contraseña = input("Contraseña: ")
     
     # Verificar credenciales
-    if usuario in usuarios and usuarios[usuario] == contraseña:
-        print(f"¡Bienvenido, {usuario}!")
-        return True  # Inicio de sesión exitoso
+    if usuario_nombre in usuarios and usuarios[usuario_nombre] == contraseña:
+        print(f"¡Bienvenido, {usuario_nombre}!")
+        main.menu()
     else:
         print("Usuario o contraseña incorrectos.")
-        return False  # Inicio de sesión fallido
 
 def menu_principal():
     while True:
         print("\n--- Menú Principal ---")
         print("1. Iniciar sesión")
-        print("2. Registro ")
+        print("2. Registro")
         print("3. Salir")
         
         opcion = input("Seleccione una opción: ")
         
         if opcion == "1":
-            if iniciar_sesion():
-                # Redirigir al usuario a otra parte del programa
-                print("Acceso concedido a funciones adicionales...")
-                break  # Salir del menú después de iniciar sesión
-      
+            iniciar_sesion()
         elif opcion == "2":
-             print("\n--- Registro de Usuario ---")
-             nombre = input("Ingrese su nombre: ")
-             apellido = input("Ingrese su apellido: ")
-             email = input("Ingrese su email: ")
-             usuarios.registrar_usuario_estandar(nombre, apellido, email)
+            print("\n--- Registro de Usuario ---")
+            nombre = input("Ingrese su nombre: ")
+            apellido = input("Ingrese su apellido: ")
+            email = input("Ingrese su email: ")
+            usuario.registrar_usuario_estandar(nombre, apellido, email)
         elif opcion == "3":
             print("Saliendo del programa. ¡Hasta luego!")
             break
