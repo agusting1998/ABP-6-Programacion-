31830222c13a9773b03f494cc66d0a4e86b690b4
agusting98 <133926923+agusting98@users.noreply.github.com>
2025-09-11 08:21:17 -0300

    Generar prueba test_dispositivos.py

---
+++ POO-SmartHome/tests/test_dispositivos.py
@@ -0,0 +1,25 @@
+import unittest
+import dispositivos
+
+class TestDispositivos(unittest.TestCase):
+    def setUp(self):
+        dispositivos.dispositivos.clear()
+
+    def test_agregar_dispositivo(self):
+        dispositivos.agregar_dispositivo("Luz Cocina", "luz", "apagado")
+        self.assertIn("Luz Cocina", dispositivos.dispositivos)
+
+    def test_dispositivo_duplicado(self):
+        dispositivos.agregar_dispositivo("Luz Cocina", "luz", "apagado")
+        dispositivos.agregar_dispositivo("Luz Cocina", "luz", "apagado")
+        self.assertEqual(len(dispositivos.dispositivos), 1)
+
+    def test_cambiar_estado(self):
+        dispositivos.agregar_dispositivo("TV", "electrodoméstico", "apagado")
+        dispositivos.cambiar_estado("TV", "encendido")
+        self.assertEqual(dispositivos.dispositivos["TV"]["estado"], "encendido")
+
+    def test_ver_estado(self):
+        dispositivos.agregar_dispositivo("Camara", "cámara", "apagado")
+        estado = dispositivos.buscar_dispositivo("Camara")["estado"]
+        self.assertEqual(estado, "apagado")
