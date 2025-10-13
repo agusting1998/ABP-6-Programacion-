a741a90a2fad48add70f3112f828092abb537ad3
agusting1998 <libertadoraguirre@gmail.com>
2025-10-13 00:43:59 -0300

    Cambio de "Connection.py" a "db_conn.py"

---
+++ POO-SmartHome/conn/db_conn.py
@@ -0,0 +1,17 @@
+import mysql.connector
+from mysql.connector import Error
+
+def get_connection():
+    try:
+        conexion = mysql.connector.connect(
+            host='127.0.0.1',
+            port=3306,
+            user='root',
+            password='1234',
+            database='hogar_inteligente'
+        )
+        if conexion.is_connected():
+            return conexion
+    except Error as e:
+        print(f"Error al conectar a MySQL: {e}")
+        return None
